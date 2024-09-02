e0de2c6f1b916456a5da74ad05f34a5c55815355
Tomas-Gaete <togaete@alumnos.uai.cl>
2024-09-02 12:35:16 -0400

    Tarea 1

--- user/yosoytupadre.c
+++ user/yosoytupadre.c
@@ -9,15 +9,13 @@ int main(int argc, char *argv[]) {
         printf("Usage: getancestor <n>\n");
         exit(1);
     }
-    int ancestor_0 = getancestor(0);
-    int ancestor_1 = getancestor(1);
+    //int ancestor_0 = getancestor(0); proceso actual
+    //int ancestor_1 = getancestor(1); padre del proceso
     int ancestor_2 = getancestor(2);
     int n = atoi(argv[1]);
     int ancestor_pid = getancestor(n);
-    printf("Padre del proceso obtenido por getppid: %d\n", ppid);
-    printf("Proceso actual: %d\n", ancestor_0);
-    printf("Padre del proceso: %d\n", ancestor_1);
-    printf("Abuelo del proceso: %d\n", ancestor_2);
-    printf("Ancestor PID at level %d: %d\n", n, ancestor_pid);
+    printf("Id del Padre del proceso obtenido por getppid: %d\n", ppid);
+    printf("Id del Abuelo del proceso: %d\n", ancestor_2);
+    printf("Id del Ancestro del proceso de nivel %d: %d\n", n, ancestor_pid);
     exit(0);
 }
